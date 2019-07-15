d9422a97f6a4a280f6613172fa7c4c288b657a44
jffau <jeffau13@gmail.com>
2019-07-15 17:34:19 -0400

    correctly loops over localdata to render out images of each project

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,248 @@
 var express = require('express');
 var router = express.Router();
+const projects = require('../projects');
+
+const localProjects = [
+  {
+    fields: {
+      title: 'siege',
+      slug: 'example-slug',
+      description: 'siege',
+      images: [
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/smoke.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/hibana.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        }
+      ]
+    }
+  },
+  {
+    fields: {
+      title: 'animations',
+      slug: 'example-slug2',
+      description:
+        'example paragraph here Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam impedit reiciendis delectus itaque quaerat, consequatur non obcaecati possimus dolores est?',
+      images: [
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/smoke.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/hibana.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        }
+      ]
+    }
+  },
+  {
+    fields: {
+      title: 'daily',
+      slug: 'example-slug3',
+      description:
+        'example paragraph here Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam impedit reiciendis delectus itaque quaerat, consequatur non obcaecati possimus dolores est?',
+      images: [
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/smoke.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/hibana.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        },
+        {
+          fields: {
+            url: 'assets/img/siege/landscape/ash.jpg',
+            title: 'My Title',
+            description: 'My desc'
+          }
+        }
+      ]
+    }
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,8 +250,9 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/projects', (req, res, next) => {
-  res.render('projects', { title: 'projects!' });
+  res.render('projects', { localProjects });
 });
+
 router.get('/tutorials', (req, res, next) => {
   res.render('tutorials', { title: 'tutorials!' });
 });
