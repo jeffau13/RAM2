1874bb9681b16f93f13537867144da098cb2f291
jffau <jeffau13@gmail.com>
2019-07-16 16:53:41 -0400

    created new routes and services to fetch from CMS

--- routes/projects.js
+++ routes/projects.js
@@ -1,3 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var projects = require('../services/products');
+var projects = require('../services/projects');
+
+// get /projects
+
+router.use(function(req, res, next) {
+  projects
+    .getProjects()
+    .then(projectsCollection => {
+      req.projects = projectsCollection.items;
+      console.log(
+        req.projects[0].fields.images[0].fields.photo.fields.file.url
+      );
+      next();
+    })
+    .catch(err => {
+      console.log('Error in routes/projects.js:', JSON.stringify(err, null, 2));
+    });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { projects: req.projects });
+});
+
+module.exports = router;
--- services/projects.js
+++ services/projects.js
@@ -2,7 +2,7 @@ const client = require('./contentful').client;
 
 const getProjects = query => {
   query = query || {};
-  query.content_type = 'photoGallery';
+  query.content_type = 'project';
   return client.getEntries(query);
 };
 
