a075e529117ad7e660e986fa1026e99cd3eaf09b
Jeff Au <jeffau13@gmail.com>
2019-07-17 14:32:46 -0400

    implemented resolution check to load appropriate sized thumbnail per device width

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/', projectsRouter);
+app.use('/projects', projectsRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var projects = require('../services/projects');
+var sr = require('screenres');
 
 // get /projects
 
@@ -9,6 +10,7 @@ router.use(function(req, res, next) {
     .getProjects()
     .then(projectsCollection => {
       req.projects = projectsCollection.items;
+
       next();
     })
     .catch(err => {
@@ -17,8 +19,23 @@ router.use(function(req, res, next) {
     });
 });
 
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { projects: req.projects, title: 'Projects' });
+router.use((req, res, next) => {
+  const resolution = sr.get();
+  const width = resolution[0];
+  if (width < 1000) {
+    req.width = 500;
+  } else {
+    req.width = 1920;
+  }
+  next();
+});
+
+router.get('/', function(req, res, next) {
+  res.render('projects', {
+    projects: req.projects,
+    title: 'Projects',
+    width: req.widthS
+  });
 });
 
 module.exports = router;
