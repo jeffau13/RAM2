b64ef35e7c7945bdaaf772c1b20b265cd1a1fc75
Jeff Au <jeffau13@gmail.com>
2019-08-20 15:48:50 -0400

    added Sentry error handler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
+const Sentry = require('@sentry/node');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -9,6 +10,12 @@ var indexRouter = require('./routes/index');
 var projectsRouter = require('./routes/projects');
 var tutorialsRouter = require('./routes/tutorials');
 var app = express();
+
+Sentry.init({
+  dsn: 'https://7fbad0401dd145529678fdaf9d063208@sentry.io/1536454'
+});
+
+app.use(Sentry.Handlers.requestHandler());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +30,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/projects', projectsRouter);
 app.use('/tutorials', tutorialsRouter);
+
+// app.get('/debug-sentry', function mainHandler(req, res) {
+//   throw new Error('My first Sentry error!');
+// });
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + '\n');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
