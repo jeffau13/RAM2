275078025783307e93c0b5ef99e933b34a10166f
Jeff Au <jeffau13@gmail.com>
2019-09-24 14:32:34 -0400

    added support for multiple instances of masonry layout by referencing div's id

--- public/javascripts/masonry.js
+++ public/javascripts/masonry.js
@@ -1,19 +1,31 @@
 window.onload = e => {
-  const macyInstance = Macy({
-    container: '.masonry',
-    trueOrder: false,
-    waitForImages: true,
-    margin: 0,
-    columns: 4,
-    breakAt: {
-      1200: 3,
-      940: 2,
-      750: 1
-    }
-  });
-};
+  const list = document.querySelectorAll('.masonry');
+  console.log(list);
+  let MacyInstances = [];
 
-macyInstance.runOnImageLoad(function() {
-  console.log('Every time an image loads I get fired');
-  macyInstance.recalculate(true);
-}, true);
+  list.forEach(div => {
+    console.log(div.id);
+    MacyInstances.push(
+      Macy({
+        container: `#${div.id}`,
+        trueOrder: false,
+        waitForImages: true,
+        margin: 0,
+        columns: 4,
+        breakAt: {
+          1920: 3,
+          1200: 2,
+          750: 1
+        }
+      })
+    );
+  });
+  MacyInstances.forEach(macyInstance => {
+    macyInstance.runOnImageLoad(function() {
+      console.log('Every time an image loads I get fired');
+      macyInstance.recalculate(true);
+    }, true);
+  });
+
+  // console.log(MacyInstances);
+};
